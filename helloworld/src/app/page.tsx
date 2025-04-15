56b6e2864669d9c0f3a9abe8ebb97143f43d7797
Akmal <akmal.okebgt@gmail.com>
2025-04-15 16:23:13 +0700

    first commit

---
+++ helloworld/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className="hero">
+        <h1>Halo, aku Akmal! 👋</h1>
+        <p>Selamat datang di website perkenalanku.</p>
+      </div>
+
+  
+    </main>
+  );
+}
