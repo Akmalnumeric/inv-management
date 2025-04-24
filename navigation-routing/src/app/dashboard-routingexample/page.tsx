96f8068e0574ba0855b4afc931c61619b9adf349
Akmalnumeric <akmal.okebgt@gmail.com>
2025-04-24 21:44:54 +0700

    Delete navigation-routing directory

--- navigation-routing/src/app/dashboard-routingexample/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-    return (
-      <main>
-        <h1>useRoute bersifat kondisional, kalau nama dan password mu benar, kamu akan diarahkan ke halaman ini!</h1>
-      </main>
-    );
-  }
-  
